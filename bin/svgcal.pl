1cd46f09bb9a18ef20f0bde738253d3fd6ea6072
Ivan Wills <ivan.wills@gmail.com>
2014-12-25 04:34:24 +1100

    Updating for release of version 0.3.5

--- bin/svgcal.pl
+++ bin/svgcal.pl
@@ -16,7 +16,7 @@ use Config::Std;
 use Data::Dumper qw/Dumper/;
 use SVG::Calendar;
 
-our $VERSION = version->new('0.3.4');
+our $VERSION = version->new('0.3.5');
 
 my %option = (
     moon     => {},
@@ -287,7 +287,7 @@ svgcal.pl - Creates the pages for a calendar in SVG format
 
 =head1 VERSION
 
-This documentation refers to svgcal.pl version 0.3.4.
+This documentation refers to svgcal.pl version 0.3.5.
 
 =head1 SYNOPSIS
 
